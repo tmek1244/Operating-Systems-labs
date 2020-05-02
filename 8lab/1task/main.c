f481e787341b72a3868832e7f98c624f84f051a2
tmek1244 <tmek1244@wp.pl>
2020-05-03 01:48:30 +0200

    lab8 done in one day wow

---
+++ 8lab/1task/main.c
@@ -0,0 +1,306 @@
+#include <math.h>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/times.h>
+#include <sys/time.h>
+
+#define buff_size 512
+#define MAX_THREADS_NUM 8
+
+const int M = 255;
+const char * delims = " \t\n\r";
+typedef struct timeval timeval;
+
+int thread_number;//       = 4;
+char * distribution_type;     // block / interleaved
+char * image_input;//  = "apple.pgm";
+char * output_file;
+
+int **picture;
+int height, width;
+int **result;
+
+
+timeval threads_times[MAX_THREADS_NUM];
+
+struct thread_info {
+    int begin;
+    int additional_value;
+    int id;
+};
+
+struct thread_info **threads_info;
+
+
+timeval gettime(){
+    timeval time;
+    gettimeofday(&time, NULL);
+    return time;
+}
+
+timeval diff_time(timeval s_time, timeval e_time){
+    timeval diff;
+    diff.tv_sec = e_time.tv_sec - s_time.tv_sec;
+    diff.tv_usec = e_time.tv_usec - s_time.tv_usec;
+
+    if (e_time.tv_usec < s_time.tv_usec){
+        --diff.tv_sec;
+        diff.tv_usec = - diff.tv_usec;
+    }
+    return diff;
+}
+
+void save_picture(int w, int h, int ** J, FILE * fp){
+    fprintf(fp, "P2\n");
+    fprintf(fp, "%d %d\n", w, h);
+    fprintf(fp, "%d\n", 255);
+
+    for (int i = 0; i < h; ++i){
+        int j;
+        for (j = 0; j < w-1; ++j){
+            fprintf(fp, "%d ", J[i][j]);
+        }
+        fprintf(fp, "%d\n", J[i][j]);
+    }
+
+    fclose(fp);
+}
+
+void save_results(timeval time, int c, const char * way_of_division){ // czasy
+    FILE *fp = fopen("Times.txt", "a");
+    fprintf(fp, "####### START ########\n");
+    if (strcmp(way_of_division, "block") == 0)      fprintf(fp, "BLOCKED mode.\n");
+    else                                            fprintf(fp, "INTERLEAVED mode.\n");
+    fprintf(fp, "%d threads with c = %d worked for %ld.%06ld\n", thread_number, c, time.tv_sec, time.tv_usec);
+    for (int i = 0; i < thread_number; ++i){
+        fprintf(fp, "\t%d-th thread worked for %ld.%ld\n", i, threads_times[i].tv_sec, threads_times[i].tv_usec);
+    }
+    fprintf(fp, "######## END #########\n\n");
+    fclose(fp);
+}
+
+
+
+
+void read_image(FILE* image)
+{
+    char str[10];
+    fscanf(image, "%s", str);
+    fscanf(image, "%d %d", &width, &height);
+    fscanf(image, "%s", str);
+    picture = (int**) calloc((size_t) height, sizeof(int *));
+    for (int i = 0; i < height; i++) {
+        picture[i] = (int*) calloc((size_t) width, sizeof(int));
+    }
+    for(int i = 0; i < height; i++)
+        for(int j = 0; j < width; j++)
+            picture[i][j] = 0;
+    for(int i = 0; i < height; i++)
+        for(int j = 0; j < width; j++)
+            fscanf(image, "%d", &picture[i][j]);
+}
+
+void free_2D_array(int **array)
+{
+    for(int i = 0; i < height; i++)
+        free(array[i]);
+    free(array);
+}
+
+void set_up_distribution()
+{
+    if(strcmp(distribution_type, "sign") == 0)
+    {
+        int section_size = (M + 1)/thread_number;
+        int modulo = (M + 1)%thread_number;
+
+        threads_info[0] = malloc(sizeof(struct thread_info));
+        threads_info[0]->begin = 0;
+        threads_info[0]->additional_value = modulo > 0 ? section_size + 1 : section_size;
+        threads_info[0]->id = 0;
+        for (int i = 1; i < thread_number; i++) {
+            threads_info[i] = malloc(sizeof(struct thread_info));
+            threads_info[i]->begin = threads_info[i - 1]->begin + threads_info[i - 1]->additional_value;
+            threads_info[i]->additional_value = modulo > i ? section_size + 1 : section_size;
+            threads_info[i]->id = i;
+        }
+    }
+    else if(strcmp(distribution_type, "block") == 0)
+    {
+        for(int i = 0; i < thread_number; i++)
+        {
+            threads_info[i] = malloc(sizeof(struct thread_info));
+            threads_info[i]->begin = (i) * (int)(ceil((width + 0.0) / thread_number));
+            threads_info[i]->additional_value = (i+1) * (int)(ceil((width + 0.0) / thread_number));
+            threads_info[i]->id = i;
+        }
+
+    } else if(strcmp(distribution_type, "interleaved") == 0)
+    {
+        for(int i = 0; i < thread_number; i++)
+        {
+            threads_info[i] = malloc(sizeof(struct thread_info));
+            threads_info[i]->begin = i;
+            threads_info[i]->additional_value = thread_number;
+            threads_info[i]->id = i;
+        }
+
+    }else {
+        printf("Sposoby podzialu: sign / block / interleaved\n");
+        free_2D_array(picture);
+        exit(-1);
+    }
+}
+
+
+void* sign(void* info)
+{
+    clock_t t;
+    t = clock();
+    struct thread_info *thread_info = (struct thread_info *) info;
+//    printf("PRZEDZIAL: %d %d\n", thread_info->begin, thread_info->additional_value);
+//    perror("sign");
+    for(int i = 0; i < width; i++)
+    {
+        for(int j = 0; j < height; j++)
+        {
+//            printf("%d    %d\n", picture[j][i], result[thread_info->id][picture[j][i]]);
+//            pintf
+            if(picture[j][i] >= thread_info->begin && picture[j][i] < thread_info->additional_value + thread_info->begin)
+                result[thread_info->id][picture[j][i]]++;
+//            perror("update");
+        }
+    }
+    double* time = malloc(sizeof(double));
+    (*time) = (((double)clock() - t)/CLOCKS_PER_SEC);
+    return time;
+}
+
+void* block(void* info)
+{
+    clock_t t;
+    t = clock();
+    struct thread_info *thread_info = (struct thread_info *) info;
+
+    for(int i = thread_info->begin; i < thread_info->additional_value; i++)
+    {
+        for(int j = 0; j < height; j++)
+        {
+//            printf("%d %d\n", j, i);
+            result[thread_info->id][picture[j][i]]++;
+        }
+    }
+    double* time = malloc(sizeof(double));
+    (*time) = (((double)clock() - t)/CLOCKS_PER_SEC);
+    return time;
+}
+
+void* interleaved(void* info)
+{
+    clock_t t;
+    t = clock();
+    struct thread_info *thread_info = (struct thread_info *) info;
+    for(int i = thread_info->begin; i < width; i += thread_info->additional_value)
+    {
+        for(int j = 0; j < height; j++)
+        {
+//            printf("%d %d\n", j, i);
+            result[thread_info->id][picture[j][i]]++;
+        }
+    }
+    double* time = malloc(sizeof(double));
+    (*time) = (((double)clock() - t)/CLOCKS_PER_SEC);
+    return time;
+}
+
+int main(int argc, char **argv) {
+    if (argc != 5)
+    {
+        printf("Poprawna skladnia: <liczba watkow> <sposob podzialu> <plik_in> <plik_out>\n");
+        return -1;
+    }
+
+    thread_number    = atoi(argv[1]);
+    distribution_type  = argv[2];
+    image_input  = argv[3];
+    output_file = argv[4];
+    FILE* image = fopen(image_input, "r");
+    FILE* output = fopen(output_file, "w");
+    if(image == NULL)
+    {
+        printf("Nie udalo sie otworzyc zdjecia!\n");
+        return -2;
+    }
+    if(output == NULL)
+    {
+        printf("Nie udalo sie otworzyc pliku wyjsciowego!\n");
+        return -2;
+    }
+
+    read_image(image);
+    clock_t t;
+    t = clock();
+    threads_info = malloc(thread_number * sizeof( struct thread_info *));
+    set_up_distribution();
+    pthread_t *thread = calloc((size_t) thread_number, sizeof(pthread_t));
+
+    result = (int **)malloc(thread_number * sizeof(int *));
+    for (int i = 0; i < thread_number; i++){
+        result[i] = (int *)malloc(256 * sizeof(int));
+        for(int j = 0; j < 256; j++)
+            (result[i][j]) = 0;
+    }
+
+    void* function = NULL;
+    if(strcmp(distribution_type, "sign") == 0)
+    {
+        function = sign;
+    }
+    else if(strcmp(distribution_type, "block") == 0)
+    {
+        function = block;
+    } else if(strcmp(distribution_type, "interleaved") == 0)
+    {
+        function = interleaved;
+    }
+
+    for (int i = 0; i < thread_number; i++) {
+        pthread_create(&thread[i], NULL, function, (void *) threads_info[i]);
+    }
+
+    for (int i = 0; i < thread_number; i++) {
+        void* time = NULL;
+        pthread_join(thread[i], &time);
+        printf("WATEK ID: %lu CZAS: %f\n", thread[i], *(double *)time);
+        free(threads_info[i]);
+    }
+
+
+    int final_result[256];
+    for(int i = 0; i < 256; i++)
+        final_result[i] = 0;
+    for(int i = 0; i < thread_number; i++)
+    {
+        for(int j = 0; j < 256; j++)
+        {
+            final_result[j] += result[i][j];
+        }
+    }
+
+    printf("CAŁKOWITY CZAS: %f\n", ((double)clock() - t)/CLOCKS_PER_SEC);
+    for(int i = 0; i < 256; i++)
+    {
+        fprintf(output,"kolor nr %d, wystapil %d razy\n", i, final_result[i]);
+    }
+//
+//    free_2D_array(result);
+    fclose(image);
+    fclose(output);
+    free_2D_array(picture);
+    return 0;
+}
